d57bcf85c32e6b1a8fa01a582d63a9674993af66
Tanaziya98 <tanaziyamb02@gmail.com>
2020-09-25 12:13:04 +0530

    init commit

---
+++ project/app/urls.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.urls import path,include
+from app import views
+
+urlpatterns = [
+   path('',views.index,name="index"),
+   path('handleblog',views.handleblog,name="handleblog"),
+   path('services',views.services,name="services"),
+   path('about',views.about,name="about"),
+   path('contact',views.contact,name="contact"),
+   path('signup',views.signup,name="signup"),
+   path('login',views.handlelogin,name="handlelogin"),
+   path('logout',views.handlelogout,name="handlelogout"),
+]
---
+++ project/app/views.py
@@ -0,0 +1,77 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.conf import settings
+from django.contrib import messages
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def handleblog(request):
+    if  not request.user.is_authenticated:
+        messages.error(request,"Please Login  & Try Again")
+        return redirect('/login')
+        messages.info(request,"Welcome To My Website")
+        return redirect('/')
+    else:
+        messages.warning(request,"Invalid Credentials")
+        return redirect('/login')
+
+    return render(request,'handleblog.html')    
+
+def services(request):
+    return render(request,'services.html')    
+
+def about(request):
+    return render(request,'about.html')    
+
+def contact(request):
+    return render(request,'contact.html')    
+
+def signup(request):
+    if request.method=="POST":
+        username=request.POST.get('username')
+        firstname=request.POST.get('firstname')
+        lastname=request.POST.get('lastname')
+        email=request.POST.get('email')
+        pass1=request.POST.get('pass1')
+        pass2=request.POST.get('pass2')
+        if pass1!=pass2:
+            return HttpResponse("Password not matched")
+
+        try:
+            if User.objects.get(username=username):
+                return HttpResponse("Username Already been Taken")    
+        except Exception as identifier:
+            pass     
+        try:
+            if User.objects.get(email=email):
+                return HttpResponse("Email already exist")    
+        except Exception as identifier:
+            pass        
+        myuser=User.objects.create_user(username,email,pass1)
+        myuser.first_name=firstname
+        myuser.last_name=lastname
+        myuser.save()
+        return HttpResponse("Sign Up Successfull")    
+
+
+    return render(request,'auth/signup.html')    
+
+def handlelogin(request):
+    if request.method=="POST":
+        handleusername=request.POST.get('username')
+        handlepassword=request.POST.get('pass1')
+        user=authenticate(username=handleusername,password=handlepassword)
+        if user is not None:
+            return redirect('/')
+        else:
+            return HttpResponse("Invalid Credentials")  
+    return render(request,'auth/login.html')
+
+
+def handlelogout(request):
+    logout(request)
+    messages.success(request,"Logout Success")
+    return redirect('/login')
