9201d2777103995e94340a4c9a593a6883b4fb3c
Tanaziya98 <tanaziyamb02@gmail.com>
2020-10-05 16:50:48 +0530

    img

--- project/project/settings.py
+++ project/project/settings.py
@@ -111,7 +111,7 @@ SOCIALACCOUNT_PROVIDERS = {
 
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER  = 'yourremail@gmail.com'
+EMAIL_HOST_USER  = 'tanaziyamb02@gmail.com'
 EMAIL_HOST_PASSWORD = 'your password'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
